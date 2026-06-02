439a04b539f36197391a0b620dbdd83dc7611f6a
Adebiyi Blessing <yoyoplenty@gmail.com>
2026-06-02 09:29:20 +0100

    Code update for omcre

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { TermiiService } from '@on/services/termii/service';
 
+import { RoleModule } from '../role/role.module';
+
 import { Token, TokenSchema } from './model/token.model';
 import { User, UserSchema } from './model/user.model';
 import { TokenRepository } from './repository/token.repository';
 import { UserRepository } from './repository/user.repository';
+import { UserSeeder } from './seeder/seeder';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
@@ -16,9 +19,10 @@ import { UserService } from './user.service';
       { name: User.name, schema: UserSchema },
       { name: Token.name, schema: TokenSchema },
     ]),
+    RoleModule,
   ],
   controllers: [UserController],
-  providers: [UserRepository, TokenRepository, UserService, TermiiService],
-  exports: [UserRepository, TokenRepository, UserService],
+  providers: [UserService, UserSeeder, UserRepository, TokenRepository, TermiiService],
+  exports: [UserService, UserRepository, TokenRepository],
 })
 export class UserModule {}
